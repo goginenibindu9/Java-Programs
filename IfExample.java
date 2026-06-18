ff85f9f4f37bfec645e2eeb7ea6ddd047a379f40
goginenibindu9 <goginenibindu9@gmail.com>
2026-06-18 21:48:03 +0530

    loops

---
+++ IfExample.java
@@ -0,0 +1,12 @@
+ class IfExample {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        }
+
+        System.out.println("Program Ended");
+    }
+}
