81956ef2caec910ba16de135364bcfcc5fc35091
goginenibindu9 <goginenibindu9@gmail.com>
2026-06-25 13:32:46 +0530

    Server-Client lab 10

---
+++ Lab-Programs/Client.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+
+        try (
+            Socket socket = new Socket("localhost", 5000);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
+        ) {
+            out.println("Hello Server!");
+
+            String reply = in.readLine();
+            System.out.println("Server: " + reply);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
+
+
+
